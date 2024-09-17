25000c84c5c4a04e99552aba2a9d44b319c34fe3
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-09-17 13:07:37 +0100

    add admin interface, present data from the database on the home page

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status") # fields from database
+    list_filter = ("status",)
+    search_fields = ("meal", "description")
+
+
+admin.site.register(Item, MenuItemAdmin)
